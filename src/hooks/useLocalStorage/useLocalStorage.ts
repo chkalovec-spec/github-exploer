f96117b3e95d61489d124508c465424af73f1939
Korev Dmitriy <korev.dmitriy@mail.ru>
2022-06-30 00:02:15 +0300

    fix useLocalStorage hook

--- src/hooks/useLocalStorage/useLocalStorage.ts
+++ src/hooks/useLocalStorage/useLocalStorage.ts
@@ -5,8 +5,15 @@ export const useLocalStorage = <T = string>(
   initialValue: T,
 ): [T, (value: T) => void] => {
   const getInitialStoredValue = () => {
-    const storageItem = window.localStorage.getItem(key)
-    return storageItem ? (JSON.parse(JSON.stringify(storageItem)) as T) : initialValue
+    const storageItem = window.localStorage.getItem(key) || ''
+    let result
+    try {
+      result = JSON.parse(storageItem) as T
+    } catch (err) {
+      result = JSON.parse(JSON.stringify(storageItem)) as T
+    } finally {
+      return storageItem ? (result as T) : initialValue
+    }
   }
 
   const [storedValue, setStoredValue] = useState(getInitialStoredValue())
@@ -15,8 +22,6 @@ export const useLocalStorage = <T = string>(
     setStoredValue(value)
     window.localStorage.setItem(key, JSON.stringify(value))
   }
-
-  console.log('updateValue', storedValue)
 
   return [storedValue, setValue]
 }
